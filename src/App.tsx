81a4fe1b7c8c5a88ff09422441229bb403791d9d
Brennan Fife <brenfife@gmail.com>
2021-12-28 16:36:02 -0600

    -

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Decentraland from './common/Maps/Decentraland';
 // import SomniumSpace from './common/Maps/SomniumSpace';
 import TheSandbox from './common/Maps/TheSandbox';
 import React from 'react';
+import theme from './theme';
+import { ChakraProvider } from '@chakra-ui/react';
 
 export default function App() {
   const worlds = [
@@ -16,22 +18,24 @@ export default function App() {
   console.log('world:', world);
 
   return (
-    <Flex direction="column" w="100%" h="calc(100vh - 60px)" mt="60px">
-      <Flex h="100%">
-        <Box w="60%" h="100%">
-          {world === 'decentraland' ? (
-            <Decentraland withControls={true} />
-          ) : world === 'the-sandbox' ? (
-            <TheSandbox />
-          ) : // : world === 'somnium-space' ? (
-          //   <SomniumSpace />
-          // ) : world === 'cryptovoxels' ? (
-          //   <Cryptovoxels />
-          // )
-          null}
-        </Box>
-        <Box w="40%" bg="red.200" />
+    <ChakraProvider theme={theme}>
+      <Flex direction="column" w="100%" h="calc(100vh - 60px)" mt="60px">
+        <Flex h="100%">
+          <Box w="60%" h="100%">
+            {world === 'decentraland' ? (
+              <Decentraland withControls={true} />
+            ) : world === 'the-sandbox' ? (
+              <TheSandbox />
+            ) : // : world === 'somnium-space' ? (
+            //   <SomniumSpace />
+            // ) : world === 'cryptovoxels' ? (
+            //   <Cryptovoxels />
+            // )
+            null}
+          </Box>
+          <Box w="40%" bg="red.200" />
+        </Flex>
       </Flex>
-    </Flex>
+    </ChakraProvider>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
 // import { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import theme from './theme';
-import { ChakraProvider } from '@chakra-ui/react';
-import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import theme from './theme';
+// import { ChakraProvider } from '@chakra-ui/react';
+// import React from 'react';
 
-ReactDOM.render(
-  <>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
-  </>,
-  document.getElementById('root'),
-);
+// ReactDOM.render(
+//   <>
+//     <ChakraProvider theme={theme}>
+//       <App />
+//     </ChakraProvider>
+//   </>,
+//   document.getElementById('root'),
+// );
+
+export * from './App';
